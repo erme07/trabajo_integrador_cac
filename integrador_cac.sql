3198a9a0006e2f004db54924b71d4e511e89de7c
erme07 <erik.medina07@gmail.com>
2023-12-18 18:29:49 -0300

    update

--- integrador_cac.sql
+++ integrador_cac.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2023 a las 00:24:10
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Dec 18, 2023 at 09:21 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,59 +18,98 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `integrador_cac`
+-- Database: `proyecto_integrador_cac`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `oradores`
+-- Table structure for table `oradores`
 --
 
 CREATE TABLE `oradores` (
   `id_orador` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
-  `mail` varchar(100) NOT NULL,
+  `mail` varchar(50) NOT NULL,
   `tema` varchar(255) NOT NULL,
-  `fecha_alta` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `descripcion` text NOT NULL,
+  `fecha_alta` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `imagen` varchar(255) NOT NULL,
+  `categorias` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `oradores`
+-- Dumping data for table `oradores`
 --
 
-INSERT INTO `oradores` (`id_orador`, `nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES
-(1, 'Steve', 'Jobs', 'stevejobs@apple.com', 'Sobre javascript', '2023-11-12 20:05:31'),
-(2, 'Bill', 'Gates', 'billgates@microsoft.com', 'Sobre javascript', '2023-11-12 20:09:17'),
-(3, 'Ada', 'Lovelace', 'adalovelace@gmail.com', 'Sobre startup', '2023-11-12 20:12:04'),
-(4, 'Juan', 'Gomes', 'jgomes@gmail.com', 'Sobre bootstrap', '2023-11-12 20:12:04'),
-(5, 'Ivan', 'gonzales', 'ivangonz@gmail.com', 'Sobre react', '2023-11-12 20:17:44'),
-(6, 'Adriana', 'Perez', 'adri.perez@gmail.com', 'Sobre typescript', '2023-11-12 20:17:44'),
-(7, 'Matias', 'Centurion', 'maticenturion@gmail.com', 'Sobre angular', '2023-11-12 20:17:44'),
-(8, 'Luca', 'Martinez', 'martinezluca@gmail.com', 'Sobre php', '2023-11-12 20:17:44'),
-(9, 'Milagros', 'Salinas', 'milisalinas@gmail.com', 'Sobre java', '2023-11-12 20:17:44'),
-(10, 'Santiago', 'Lopez', 'san_tiagolopez@gmail.com', 'Sobre ruby', '2023-11-12 20:17:44');
+INSERT INTO `oradores` (`id_orador`, `nombre`, `apellido`, `mail`, `tema`, `descripcion`, `fecha_alta`, `imagen`, `categorias`) VALUES
+(1, 'Jonathan', 'Mircha', 'jmircha@gmail.com', 'Introduccion a las \"Progressive web apps\"', 'Las PWAs (Progressive Web Apps) están cambiando la forma en que interactuamos en la web. Descubre como fusionan velocidad web con funciones de aplicaciones nativas.', '2023-12-10 09:52:29', 'jon-mircha.webp', 'javascript,css'),
+(2, 'Carmen', 'Ansio', 'carmen-ansio@gmail.com', 'Desarrollo de modelos creados en Figma', 'Explora el desarrollo práctico de diseños desde Figma. Aprende a dar vida a tus modelos en una charla que revela los secretos detrás de la creación y ejecución efectiva.', '2023-12-10 10:00:10', 'carmen-ansio.webp', 'figma,html,css'),
+(3, 'Kevin', 'Powell', 'kevin-powell@gmail.com', 'Las novedades en CSS4', 'Explora las últimas innovaciones con CSS4. Desde nuevas propiedades hasta mejoras en diseño responsivo, esta charla destaca las funciones que están dando forma al futuro del estilo web.', '2023-12-10 10:01:38', 'kevin-powell.webp', 'css,html'),
+(4, 'Brais', 'Moure', 'mouredev@gmail.com', 'PHP para principiantes', 'Inicia tu viaje en el desarrollo web con PHP. Esta charla introduce los conceptos básicos y necesarios para la creación de tu primer script.', '2023-12-10 10:03:31', 'mouredev.webp', 'php,laravel'),
+(5, 'Miguel', 'Duran', 'midu@gmail.com', 'Control de versiones con GIT', 'Desde la clonación hasta el commit, esta charla te guiará en el uso efectivo de GIT para gestionar y rastrear cambios en tus proyectos.', '2023-12-10 10:05:54', 'midudev.webp', 'git,github'),
+(6, 'Victor', 'Robles', 'victor.robles@gmail.com', 'Desarrollo web con Angular', 'Descubre Angular, el potente framework para desarrollo web. Esta charla te llevará a través de los fundamentos para construir aplicaciones web modernas.', '2023-12-10 10:13:49', 'victor-robles.webp', 'angular,javascript'),
+(7, 'Gonzalo', 'Pozzo', 'goncy@gmail.com', 'Documenta correctamente tu repositorio', 'Descubre cómo una documentación clara y estructurada puede potenciar tu proyecto y hacer que sea más accesible para colaboradores y usuarios. ¡Aprende a optimizar tus proyectos en GitHub con técnicas efectivas de documentación!', '2023-12-15 17:19:02', '1702671542_goncy.webp', 'github,markdown'),
+(8, 'Oscar', 'Uh', 'oscaruh@gmail.com', 'Diseño de plantillas personalizadas en Wordpress', ' Descubre cómo crear plantillas personalizadas que transformarán la apariencia de tus sitios web. Desde lo básico hasta técnicas avanzadas, esta charla te guiará en el camino para destacar en el diseño web con WordPress', '2023-12-15 17:27:54', '1702672074_oscaruh.webp', 'php,wordpress');
+
+-- --------------------------------------------------------
 
 --
--- Índices para tablas volcadas
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rol` enum('administrador','cliente') DEFAULT 'cliente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `correo`, `password`, `fecha_registro`, `rol`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2y$10$OAzeBV0PVtcDuOveeLUraeWd9TmN9TDXJyxQJE00MCaiOdx7I.eW.', '2023-12-16 23:02:56', 'administrador'),
+(2, 'fulanito', 'fulanito@gmail.com', '$2y$10$EsHh4lpEzGUADssM3J3YLuwQMPcf0Ih5Js26AP8ghewh4/JMO0IDa', '2023-12-18 16:18:17', 'cliente'),
+(4, 'julian', 'julian@gmail.com', '$2y$10$.nIQSEPFopxnaC8B29rDsOTKrvecUJCpJzAVBWu2lIXgyexAUm6O6', '2023-12-18 21:16:52', 'cliente');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `oradores`
+-- Indexes for table `oradores`
 --
 ALTER TABLE `oradores`
   ADD PRIMARY KEY (`id_orador`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `oradores`
+-- AUTO_INCREMENT for table `oradores`
 --
 ALTER TABLE `oradores`
-  MODIFY `id_orador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_orador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
